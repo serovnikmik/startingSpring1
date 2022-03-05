439dfa44f3edbc3ab9815c0345385595330d5d8c
What Git <serovnikmik@gmail.com>
2022-03-05 08:26:31 +0300

    after "Bean Lifecycle lesson"

--- src/main/java/org/example/ClassicalMusic.java
+++ src/main/java/org/example/ClassicalMusic.java
@@ -2,6 +2,22 @@ package org.example;
 
 public class ClassicalMusic implements Music{
 
+    private ClassicalMusic(){
+
+    }
+
+    public static ClassicalMusic getClassicalMusic(){
+        return new ClassicalMusic();
+    }
+
+    public void doMyInit(){
+        System.out.println("Doing my initialization (CM)");
+    }
+
+    public void doMyDestroy(){
+        System.out.println("Doing my destruction (CM)");
+    }
+
     @Override
     public String getSong(){
         return "Hungarian Rapsody";
--- src/main/java/org/example/TestSpring.java
+++ src/main/java/org/example/TestSpring.java
@@ -9,24 +9,32 @@ public class TestSpring {
                 "applicationContext.xml"
         );
 
+
+
+        ClassicalMusic cm = ClassicalMusic.getClassicalMusic();
+//        ClassicalMusic classicalMusic = context.getBean(
+//                "classicalMusicBean",
+//                ClassicalMusic.class);
+//        System.out.println(classicalMusic.getSong());
+
         //Music music = context.getBean("musicBean", Music.class);
 
         //MusicPlayer musicPlayer = new MusicPlayer(music);
 
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        System.out.println("starting volume: " + musicPlayer.getVolume());
-
-        MusicPlayer musicPlayer1 = context.getBean("musicPlayer", MusicPlayer.class);
-        musicPlayer1.setVolume(288);
-        System.out.println("result volume: " + musicPlayer.getVolume());
-        System.out.println(musicPlayer == musicPlayer1);
-
-        musicPlayer.playMusic();
-        System.out.println("player name: "
-                + musicPlayer.getName());
-        System.out.println("volume: "
-                + musicPlayer.getVolume());
-        musicPlayer.playMusicList();
+//        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        System.out.println("starting volume: " + musicPlayer.getVolume());
+//
+//        MusicPlayer musicPlayer1 = context.getBean("musicPlayer", MusicPlayer.class);
+//        musicPlayer1.setVolume(288);
+//        System.out.println("result volume: " + musicPlayer.getVolume());
+//        System.out.println(musicPlayer == musicPlayer1);
+//
+//        musicPlayer.playMusic();
+//        System.out.println("player name: "
+//                + musicPlayer.getName());
+//        System.out.println("volume: "
+//                + musicPlayer.getVolume());
+//        musicPlayer.playMusicList();
 
         context.close();
     }
